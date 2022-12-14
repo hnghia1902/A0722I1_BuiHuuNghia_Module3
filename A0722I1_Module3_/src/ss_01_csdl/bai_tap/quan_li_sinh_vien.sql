022097032de777710b83d74556eb31b6e40017af
ád ád <you@example.com>
2022-12-14 20:20:25 +0700

    ,

---
+++ A0722I1_Module3_/src/ss_01_csdl/bai_tap/quan_li_sinh_vien.sql
@@ -0,0 +1,13 @@
+create database `quan_li_sinh_vien`;
+use quan_li_sinh_vien;
+create table `class`(
+id int not null,
+`name` varchar(40) not null,
+primary key(id)
+);
+create table `Teacher`(
+id int not null,
+`name` varchar(40) not null,
+age int not null,
+country varchar(45)
+);
